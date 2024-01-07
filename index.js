8411ad0ce3b7eb597bc8e97076456d252c12a8be
sahil khan <sahilkhan2002786@gmail.com>
2024-01-07 19:36:41 +0530

    Todo routes has been added

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 const express = require('express');
 const app = express();
+// load config from env file
+const PORT = process.env.PORT || 4000;
 
-app.listen(3000,()=>{
-    console.log("Server Created at port no 3000")
-})
+// middleware to parse json request from the body
+app.use(express.json());
+
+//import route from todo api
+const todoRoutes = require("./routes/todos");
+app.use("/api/v1", todoRoutes);
+
+//server started
+app.listen(PORT, ()=>{
+    console.log(`Server Started successfully at ${PORT}`)
+});
+ 
+
+//connected to database
+const dbConnect = require("./config/database");
+dbConnect();
+ 
+
+// default route
+app.get('/',(req,res)=>{
+    res.send('<h1> This is homepage </h1>')
+});
